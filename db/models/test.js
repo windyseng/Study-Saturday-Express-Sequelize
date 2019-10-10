7f13e5ef09ba4e04ae4c1ccdcab2bce81b29fcff
Windy <windy.seng@gmail.com>
2019-10-10 16:03:52 -0400

    finished student and half of test

--- db/models/test.js
+++ db/models/test.js
@@ -3,6 +3,19 @@ const Sequelize = require('sequelize');
 const db = require('../db');
 const Student = require('./student');
 
-const Test = db.define();
+const Test = db.define('tests', {
+  subject: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  grade: {
+    type: Sequelize.NUMBER,
+    allowNull: false
+  },
+  subject: {
+    type: Sequelize.TEXT,
+    allowNull: false
+  }
+});
 
 module.exports = Test;
